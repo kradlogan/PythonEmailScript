5479dcd43959f44392522d57df2a39f3147fd984
kradlogan <kradlogan@gmail.com>
2014-03-08 13:01:21 -0600

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,10 +3,10 @@
 import smtplib
 
 sender = 'from@fromdomain.com'
-receivers = ['kradlogan@gmail.com']
+receivers = ['reciever@recieve.net']
 
 message = """From: James Frank <jamesfrank@crazyppl.org>
-To: To Person <kradlogan@gmail.com>
+To: To Person <reciever@recieve.net>
 MIME-Version: 1.0
 Content-type: text/html
 Subject: SMTP email test
